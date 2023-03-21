c4e61ed76f2f1d1c27329208c9261980e4b62a34
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-20 20:46:03 -0500

    Fix Imports

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from 'src/users/users.module';
 import { UsersProjectsEntity } from '../users/entities/usersProjects.entity';
+import { UsersModule } from '../users/users.module';
 import { ProjectsController } from './controllers/projects.controller';
 import { ProjectEntity } from './entities/project.entity';
 import { ProjectsService } from './services/projects.service';
